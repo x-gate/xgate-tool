5c1637b7b47f210171eb3008d81169a19b07f917
Vincent Chi <song374561@chivincent.net>
2020-02-24 11:31:21 +0800

    test: create test cases for checking if serialize and deserialize can work for pixel.

--- src/data_structure/graphic.rs
+++ src/data_structure/graphic.rs
@@ -40,7 +40,7 @@ pub struct GraphicV2 {
     pub palette_data: Palette
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
 pub struct Pixel {
     pub r: u8,
     pub g: u8,
@@ -64,6 +64,7 @@ mod test {
         assert!(bytes.iter().eq(bincode::serialize(&graphic_info).unwrap().iter()));
     }
 
+        
     #[test]
     fn deserialize_graphic_info() {
         let bytes = [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xa8, 0x01, 0x00, 0x00, 0xe0, 0xff, 0xff, 0xff, 0xe8, 0xff, 0xff, 0xff, 0x40, 0x00, 0x00, 0x00, 0x2f, 0x00, 0x00, 0x00, 0x01, 0x01, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0xe7, 0x03, 0x00, 0x00];
@@ -76,7 +77,7 @@ mod test {
     }
 
     #[test]
-    fn deserialize_graphic_header() {
+    fn serialize_graphic_header() {
         let graphic_header = GraphicHeader {
             mark: ['R', 'D'], version: 1, unknown: 16, width: 64, height: 47, length: 424,
         };
@@ -84,9 +85,9 @@ mod test {
 
         assert_eq!(bytes, bincode::serialize(&graphic_header).unwrap().as_slice());
     }
-    
+
     #[test]
-    fn serialize_graphic_header() {
+    fn deserialize_graphic_header() {
         let bytes = [0x52, 0x44, 0x01, 0x10, 0x40, 0x00, 0x00, 0x00, 0x2f, 0x00, 0x00, 0x00, 0xa8, 0x01, 0x00, 0x00];
         let graphic_header = bincode::deserialize::<GraphicHeader>(&bytes).unwrap();
         let expect = GraphicHeader {
@@ -94,5 +95,24 @@ mod test {
         };
 
         assert_eq!(graphic_header, expect);
+    }
+
+    #[test]
+    fn serialize_pixel() {
+        let pixel = Pixel {
+            r: 0, g: 0, b: 0
+        };
+        let bytes = [0x00, 0x00, 0x00];
+        
+        assert_eq!(bytes, bincode::serialize(&pixel).unwrap().as_slice());
+    }
+
+    #[test]
+    fn deserialize_pixel() {
+        let bytes = [0x00, 0x00, 0x00];
+        let pixel = bincode::deserialize::<Pixel>(&bytes).unwrap();
+        let expect = Pixel {r: 0, g: 0, b: 0};
+
+        assert_eq!(pixel, expect);
     }
 }
