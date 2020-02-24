fd9dfdac00f2df6da30f6ad7e940a82fd106e4f1
Vincent Chi <song374561@chivincent.net>
2020-02-24 11:12:17 +0800

    test: create test cases for checking if serialize and deserialize can work for graphic_info.

--- src/data_structure/graphic.rs
+++ src/data_structure/graphic.rs
@@ -1,6 +1,6 @@
 use serde::{Serialize, Deserialize};
 
-#[derive(Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
 pub struct GraphicInfo {
     pub id: u32,
     pub address: u32,
@@ -45,4 +45,31 @@ pub struct Pixel {
     pub b: u8,
 }
 
-pub struct Palette(Vec<Pixel>)
+pub struct Palette(Vec<Pixel>);
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use bincode;
+
+    #[test]
+    fn serialize_graphic_info() {
+        let graphic_info = GraphicInfo {
+            id: 0, address: 0, length: 424, offset_x: -32, offset_y: -24, width: 64, height: 47, tile_east: 1, tile_south: 1, access: 1, unknown: [0, 0, 0, 0, 0], map:999
+        };
+        let bytes = [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xa8, 0x01, 0x00, 0x00, 0xe0, 0xff, 0xff, 0xff, 0xe8, 0xff, 0xff, 0xff, 0x40, 0x00, 0x00, 0x00, 0x2f, 0x00, 0x00, 0x00, 0x01, 0x01, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0xe7, 0x03, 0x00, 0x00];
+
+        assert!(bytes.iter().eq(bincode::serialize(&graphic_info).unwrap().iter()));
+    }
+
+    #[test]
+    fn deserialize_graphic_info() {
+        let bytes = [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xa8, 0x01, 0x00, 0x00, 0xe0, 0xff, 0xff, 0xff, 0xe8, 0xff, 0xff, 0xff, 0x40, 0x00, 0x00, 0x00, 0x2f, 0x00, 0x00, 0x00, 0x01, 0x01, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0xe7, 0x03, 0x00, 0x00];
+        let graphic_info = bincode::deserialize::<GraphicInfo>(&bytes).unwrap();
+        let expect = GraphicInfo {
+            id: 0, address: 0, length: 424, offset_x: -32, offset_y: -24, width: 64, height: 47, tile_east: 1, tile_south: 1, access: 1, unknown: [0, 0, 0, 0, 0], map:999
+        };
+
+        assert_eq!(graphic_info, expect);
+    }
+}
