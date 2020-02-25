b08c5958415d96709e86160cf823e9fdc14f61f2
Vincent Chi <song374561@chivincent.net>
2020-02-25 13:38:34 +0800

    feat: parse graphic id from args.

--- src/features/info.rs
+++ src/features/info.rs
@@ -1,5 +1,22 @@
+use std::num;
 use crate::resource::graphic::{GraphicInfoResource, GraphicResource};
 
 pub fn show_info<T>(args: &clap::ArgMatches, resource: (GraphicInfoResource, GraphicResource, T)) {
-    
+    let result = ArgParse::parse(args);
+}
+
+struct ArgParse {
+    id: u32,
+}
+
+impl ArgParse {
+    fn parse(args: &clap::ArgMatches) -> Result<Self, num::ParseIntError> {
+        if args.value_of("id").is_none() {
+            Ok(Self {id: 0})
+        } else {
+            Ok(Self {
+                id: args.value_of("id").unwrap().parse::<u32>()?
+            })
+        }
+    }
 }
