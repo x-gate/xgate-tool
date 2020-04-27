022c7b9c2a8fd9f5d12b24da6776e2a7525b1460
Vincent Chi <song374561@chivincent.net>
2020-04-27 16:39:16 +0800

    refactor: remove Box when returning Graphic.

--- src/features/dump.rs
+++ src/features/dump.rs
@@ -21,17 +21,17 @@ pub fn dump_graphics(
         let (graphic_info, mut graphic) =
             find_by_id(result.id.unwrap(), &mut resources.0, &mut resources.1)?;
         let palette = resources.2.as_mut().unwrap().build()?;
-        debug!("{:?}", *graphic);
+        debug!("{:?}", graphic);
         debug!("{:?}", palette);
 
         info!("Decoding graphic data");
-        if (*graphic).header.version & 1 == 1 {
-            (*graphic).data = (*graphic).data.decode();
+        if graphic.header.version & 1 == 1 {
+            graphic.data = graphic.data.decode();
         }
         info!("Decoded graphic data");
 
         info!("Building image");
-        if let Some(image) = (*graphic).build_image(&graphic_info, &palette)? {
+        if let Some(image) = graphic.build_image(&graphic_info, &palette)? {
             image.save(
                 format!(
                     "{}/{}.bmp",
@@ -46,10 +46,10 @@ pub fn dump_graphics(
 
         for (info, mut graphic) in find_all(&mut resources.0, &mut resources.1)? {
             trace!("Graphic ID: {}", info.id);
-            if (*graphic).header.version & 1 == 1 {
-                (*graphic).data = (*graphic).data.decode();
+            if graphic.header.version & 1 == 1 {
+                graphic.data = graphic.data.decode();
             }
-            if let Some(image) = (*graphic).build_image(&info, &palette)? {
+            if let Some(image) = graphic.build_image(&info, &palette)? {
                 image.save(
                     format!(
                         "{}/{}.bmp",
@@ -64,13 +64,13 @@ pub fn dump_graphics(
     Ok(())
 }
 
-fn get_graphic(info: &GraphicInfo, header: &GraphicHeader, graphic: &mut GraphicResource) -> Result<Box<GraphicV1>, Box<dyn std::error::Error>> {
+fn get_graphic(info: &GraphicInfo, header: &GraphicHeader, graphic: &mut GraphicResource) -> Result<GraphicV1, Box<dyn std::error::Error>> {
     let graphic = match header.version {
         0 | 1 => {
             graphic.seek(SeekFrom::Start(info.address as u64))?;
-            Box::new(GraphicV1::new(
+            GraphicV1::new(
                 graphic.read(info.length as usize)?,
-            )?)
+            )?
         },
         2 | 3 => {
             todo!()
@@ -87,7 +87,7 @@ fn find_by_id(
     id: u32,
     graphic_info_resource: &mut GraphicInfoResource,
     graphic_resource: &mut GraphicResource,
-) -> Result<(GraphicInfo, Box<GraphicV1>), Box<dyn std::error::Error>> {
+) -> Result<(GraphicInfo, GraphicV1), Box<dyn std::error::Error>> {
     info!("Finding graphic by id = {}", id);
     let graphic_info = graphic_info_resource.find(|gi| gi.id == id).unwrap();
     debug!("Found graphic_info = {:?}", graphic_info);
@@ -104,7 +104,7 @@ fn find_by_id(
 fn find_all(
     graphic_info_resource: &mut GraphicInfoResource,
     graphic_resource: &mut GraphicResource,
-) -> Result<Vec<(GraphicInfo, Box<GraphicV1>)>, Box<dyn std::error::Error>> {
+) -> Result<Vec<(GraphicInfo, GraphicV1)>, Box<dyn std::error::Error>> {
     let mut ret = vec![];
 
     info!("Collecting all of GraphicInfo and GraphicHeader");
